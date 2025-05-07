cd1b2fd4d919f34339c6e5b0ebd43380e77a54b2
VaalGeek <ranyaole@yahoo.com>
2025-05-07 19:37:27 +0200

    fixed verification error

--- server/api/stakeholders/fetchByRole.get.ts
+++ server/api/stakeholders/fetchByRole.get.ts
@@ -4,7 +4,7 @@ import { fetchByRole } from '~/server/controllers/StakeholderController';
 
 export default defineEventHandler(async (event) => {
   const {role,schoolId } = getQuery(event);
- 
+
   try {
       return fetchByRole(role?.toString()!,schoolId?.toString()!)
   } catch (error:any) {
